3ed9b6c607cff83c0aa2f6da9b270acf31f4374c
ocxacell <lemussebastian843@gmail.com>
2025-09-21 18:39:07 -0500

    Se agrego la funcion para mostrar los nombres y asegurarse de limpiar la lista

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos=[]
-
 function asignarTextoElement(elemento,texto){
     let elementoHTML = document.getElementById(elemento);
     elementoHTML.innerHTML= texto;
@@ -15,9 +14,20 @@ function agregarAmigo(){
     amigos.push(nombre)
     console.log(amigos)
     limpiarInput('amigo')
+    mostrarAmigos()
 }
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
 function limpiarInput(elementoID){
     document.getElementById(elementoID).value=''
+}
+
+function mostrarAmigos(){
+  let lista = document.getElementById("listaAmigos");
+  lista.innerHTML = ""; 
+  amigos.forEach((nombre) => {
+    let li = document.createElement("li");
+    li.textContent = nombre;
+    lista.appendChild(li);
+    })
 }
