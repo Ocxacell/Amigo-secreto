339e3404b0094da6eec21f73c84bcb951079c2e6
ocxacell <lemussebastian843@gmail.com>
2025-09-21 17:37:33 -0500

    Se anadio la funcion para agregar amigos

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos=[]
 
+function asignarTextoElement(elemento,texto){
+    let elementoHTML = document.getElementById(elemento);
+    elementoHTML.innerHTML= texto;
+}
+
 function agregarAmigo(amigos){
     amigos.push(document.getElementById('amigo').value)
     console.log(amigos)
     return amigos
-}
-
-function asignarTextoElement(elemento,texto){
-    let elementoHTML = document.getElementById(elemento);
-    elementoHTML.innerHTML= texto;
 }
